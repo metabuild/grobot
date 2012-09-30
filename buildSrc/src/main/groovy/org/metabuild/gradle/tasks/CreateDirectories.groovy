d86a06d359ff5783e7ab3c941cc5bf1b86bc4e6a
John Burbridge <johnburbridge@gmail.com>
2012-09-29 17:07:25 -0700

    Organized build - move custom tasks to buildSrc folder Added defaults for a webapp under server (instead of Grails)

---
+++ buildSrc/src/main/groovy/org/metabuild/gradle/tasks/CreateDirectories.groovy
@@ -0,0 +1,36 @@
+package org.metabuild.gradle.tasks;
+
+import java.io.File;
+import java.util.List;
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.OutputDirectories;
+import org.gradle.api.tasks.TaskAction;
+
+import java.util.logging.Logger;
+
+
+/**
+ * Creates directori es if they don't already exist
+ *
+ * @author jburbridge
+ * @since 9/27/2012
+ */
+class CreateDirectories extends DefaultTask {
+
+	Logger logger = Logger.getLogger("CreateDirectories")
+	
+	@OutputDirectories
+	List<File> directories
+
+	@TaskAction
+	def createDirectories() {
+		directories.each { File directory ->
+			if (!directory.exists()) {
+				logger.warn("Creating ${directory}")
+				directory.mkdirs()
+			}
+		}
+	}
+}
+
