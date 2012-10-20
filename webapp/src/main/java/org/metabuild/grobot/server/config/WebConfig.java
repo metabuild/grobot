7b17b1eb414499941773f9342fde4e8c767f19cb
John Burbridge <jburbridge@salesforce.com>
2012-10-19 17:20:16 -0700

    Adding Tiles support to Spring

--- webapp/src/main/java/org/metabuild/grobot/server/config/WebConfig.java
+++ webapp/src/main/java/org/metabuild/grobot/server/config/WebConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles2.TilesConfigurer;
 
 /**
  * Spring WebMVC configuration
@@ -21,11 +25,32 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages = "org.metabuild.grobot.webapp.controllers")
 public class WebConfig {
 
+	
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver ir = new InternalResourceViewResolver();
 		ir.setPrefix("/WEB-INF/jsp/");
 		ir.setSuffix(".jsp");
 		return ir;
+	}
+	
+	@Bean(name="tilesViewResolver")
+	public ViewResolver getUrlBasedViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setViewClass(org.springframework.web.servlet.view.tiles2.TilesView.class);
+		return resolver;
+	}
+	
+	@Bean(name="tilesConfigurer")
+	public TilesConfigurer getTilesConfigurer() {
+		TilesConfigurer configurer = new TilesConfigurer();
+		String[] definitions = { "WEB-INF/layouts/layouts.xml" };
+		configurer.setDefinitions(definitions);
+		configurer.setValidateDefinitions(true);
+		return configurer;
 	}
 }
